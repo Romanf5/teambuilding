c24282318f5621e6a518769cc8006a89fcea2fb2
Romanf5 <berezhnoi.f5@gmail.com>
2018-03-02 18:28:04 +0200

    Added header, banner slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
 
+  var bannerSlider = $('#banner-slider');
+
+  init()
+
+  function init() {
+    bannerSlider.slick({
+      dots: true,
+      arrows:false,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear'
+    })
+  }
+
 });
