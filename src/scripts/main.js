09c5daa8b9c602f2b7e91473c298693661e7f9ce
Romanf5 <berezhnoi.f5@gmail.com>
2018-03-05 15:19:31 +0200

    Sticky header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function () {
   //UI var
   var bannerSlider = $('#banner-slider');
   var caseSlider = $('#js-case-slider');
+  var header = $('.page-header');
 
   init();
 
   function init() {
     bannerSlider.slick({
       dots: true,
-      arrows:false,
+      arrows: false,
       infinite: true,
       speed: 500,
       fade: true,
@@ -19,9 +20,27 @@ $(document).ready(function() {
       slidesToScroll: 5,
       infinite: true,
       dots: true,
-      arrows:false,
+      arrows: false,
       dotsClass: 'custom-dots',
     });
+    $(window).on('scroll', function () {
+      stickyHandler(header, 0);
+    });
+  }
+
+  function stickyHandler(element, startPosition) {
+    if (element.hasClass('page-header')) {
+      var headerHaight = header.height();
+      var mainWrp = $('#main-wrp');
+
+      if ($(window).scrollTop() > startPosition) {
+        mainWrp.css('padding-top', headerHaight);
+        element.addClass('sticky');
+      } else {
+        mainWrp.removeAttr('style');
+        element.removeClass('sticky')
+      }
+    }
   }
 
 });
