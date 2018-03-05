51abc7979945b42848185c3e7bf8b1789836f40b
Romanf5 <berezhnoi.f5@gmail.com>
2018-03-05 12:22:22 +0200

    Added case and man section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
-
+  //UI var
   var bannerSlider = $('#banner-slider');
+  var caseSlider = $('#js-case-slider');
 
-  init()
+  init();
 
   function init() {
     bannerSlider.slick({
@@ -12,7 +13,15 @@ $(document).ready(function() {
       speed: 500,
       fade: true,
       cssEase: 'linear'
-    })
+    });
+    caseSlider.slick({
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      infinite: true,
+      dots: true,
+      arrows:false,
+      dotsClass: 'custom-dots',
+    });
   }
 
 });
